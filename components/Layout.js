a2dbd906e552c2b1790e463742fa39d7c7816dc7
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-04 17:15:37 -0300

    Tailwind integration done!

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Layout({title, children}) {
+  return (
+    <div className="bg-gray-300">
+        <Head>
+            <title>{title}</title>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="container mx-auto max-w-xl pt-8 min-h-screen">
+            {children}
+        </main>
+    </div>
+  )
+}
