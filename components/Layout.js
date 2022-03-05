d94d9f79c393cec632cfbff9f9c65c122f0fe740
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-05 20:29:34 -0300

    Changing page icon and background color, adding background image

--- components/Layout.js
+++ components/Layout.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Head from 'next/head'
 
 export default function Layout({title, children}) {
+  //const randomBackground = Math.floor(Math.random() * 4)
+
   return (
-    <div className="bg-gray-300">
+    <div className="bg-[url('../public/background/electric-pokemon.png')]">
         <Head>
             <title>{title}</title>
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/pokeball-icon.ico" />
         </Head>
-        <main className="container mx-auto max-w-xl pt-8 min-h-screen">
+        <main className="container mx-auto max-w-xl pt-8 min-h-screen bg-gray-300 bg-opacity-80 px-6">
             {children}
         </main>
     </div>
