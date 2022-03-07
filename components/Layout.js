9abbd2ffa1adf1cc1e6a2354f2be7ff898bfba3f
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-07 14:28:15 -0300

    Adding <meta/> tag with description for page preview

--- components/Layout.js
+++ components/Layout.js
@@ -9,6 +9,7 @@ export default function Layout({title, children}) {
         <Head>
             <title>{title}</title>
             <link rel="icon" href="/pokeball-icon.ico" />
+            <meta name="description" content="Pokedex including the first 151 Pokemon - Gen 1. Made with NextJS with Tailwind and PokeAPI."/>
         </Head>
         <main className="container mx-auto max-w-xs sm:max-w-xl p-8 min-h-screen bg-gray-300 bg-opacity-80">
             {children}
