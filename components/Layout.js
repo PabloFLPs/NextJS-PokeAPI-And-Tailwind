05fa0e83fcb6458b6984e3ba5af586753841aa95
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-08 14:29:26 -0300

    Changing url image to Github RAW content link

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ export default function Layout({title, children}) {
             <title>{title}</title>
             <link rel="icon" href="/pokeball-icon.ico" />
             <meta name="description" content="Pokedex including all 898 Pokemon - 8 Generations. Made with NextJS with Tailwind and PokeAPI."/>
-            <meta property="og:image" content="https://github.com/PabloFLPs/NextJS-PokeAPI-And-Tailwind/blob/master/public/pokeball-icon.png"/>
+            <meta property="og:image" content="https://raw.githubusercontent.com/PabloFLPs/NextJS-PokeAPI-And-Tailwind/master/public/pokeball-icon.png"/>
         </Head>
         <main className="container mx-auto max-w-xs sm:max-w-xl p-8 min-h-screen bg-gray-300 bg-opacity-80">
             {children}
