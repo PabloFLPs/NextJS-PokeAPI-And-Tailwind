36856a7b5517b20c196c97460eb6287e2449784b
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-03-08 16:17:54 -0300

    Changing bottom padding to 2px

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ export default function Layout({title, children}) {
             <meta name="description" content="Pokedex including all 898 Pokemon - 8 Generations. Made with NextJS with Tailwind and PokeAPI."/>
             <meta property="og:image" content="https://raw.githubusercontent.com/PabloFLPs/NextJS-PokeAPI-And-Tailwind/master/public/pokeball-icon.png"/>
         </Head>
-        <main className="container mx-auto max-w-xs sm:max-w-xl p-8 min-h-screen bg-gray-300 bg-opacity-80">
+        <main className="container mx-auto max-w-xs sm:max-w-xl p-8 pb-2 min-h-screen bg-gray-300 bg-opacity-80">
             {children}
         </main>
     </div>
